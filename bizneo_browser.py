66fc4aecee6dbc01044f27c2e61d9b1fff03c663
Miguel Julián <figarocorso@users.noreply.github.com>
2024-07-12 16:47:49 +0200

    Refactor and avoid crash if schedule already exists (#4)
    * Refactor add default schedule with browswer
    
    * Avoid crash if schedule already exists

--- bizneo_browser.py
+++ bizneo_browser.py
@@ -1,11 +1,6 @@
 import argparse
-from os import path
-from glob import glob
 from datetime import datetime
-from playwright.sync_api import sync_playwright
-
-
-PROFILE_PATH = ""
+from src.browser.library import add_expected_schedule
 
 
 def parse_args():
@@ -20,50 +15,6 @@ def parse_args():
     return args
 
 
-def get_default_firefox_profile():
-    profile_path = path.expanduser("~/Library/Application Support/Firefox/Profiles")
-    if not path.exists(profile_path):
-        raise Exception("Firefox profiles directory not found.")
-
-    default_profiles = glob(path.join(profile_path, "*.default"))
-    if not default_profiles:
-        raise Exception("Default profile not found.")
-
-    return default_profiles[0]
-
-
-def main(args):
-    with sync_playwright() as p:
-        firefox = p.firefox
-        browser = firefox.launch_persistent_context(
-            user_data_dir=PROFILE_PATH or get_default_firefox_profile(),
-            headless=args.headless,
-            args=["--new-tab"],
-        )
-        page = browser.pages[0]
-        page.goto("https://sysdig.bizneohr.com")
-        time_xpath = "//a[contains(@class, 'menu-link')][contains(@data-active-link, 'time-attendance')]"
-        user_id = page.locator(time_xpath).get_attribute("href").split("/")[-1]
-        year = args.date.year
-        month = args.date.month
-        day = args.date.day
-        print(f"Adding expected schedule for user {user_id} at {year}-{month}-{day})")
-        page.goto(f"https://sysdig.bizneohr.com/time-attendance/my-logs/{user_id}?year={year}&month={month}")
-        page.reload()
-        page.locator(f"//tr[@data-bulk-element='{day}']/td[@class='actions']").click()
-        locator = page.locator(
-            f"//form[contains(@action, '={day}')]//button[contains(@class, 'is-link')][contains(text(), 'Añadir jornada esperada')]"  # noqa
-        )
-        for element in locator.all():
-            if element.is_visible():
-                element.click()
-        page.locator("//button[contains(text(), 'Confirmar')]").click()
-        page.wait_for_selector("//*[contains(text(), 'Has añadido con éxito')]").wait_for_element_state(
-            "visible"
-        )
-        browser.close()
-
-
 if __name__ == "__main__":
     args = parse_args()
-    main(args)
+    add_expected_schedule(args)
---
+++ src/browser/library.py
@@ -0,0 +1,65 @@
+from os import path
+from glob import glob
+from playwright.sync_api import sync_playwright
+
+
+PROFILE_PATH = ""
+
+
+def add_expected_schedule(args):
+    with sync_playwright() as playwright:
+        browser, page = get_browser_and_page(playwright, args)
+        user_id = get_current_user_id(page)
+        year, month, day = (args.date.year, args.date.month, args.date.day)
+        add_expected_schedule_at_date_for_user(page, user_id, year, month, day)
+        browser.close()
+
+
+def get_browser_and_page(playwright, args):
+    firefox = playwright.firefox
+    browser = firefox.launch_persistent_context(
+        user_data_dir=PROFILE_PATH or _get_default_firefox_profile(),
+        headless=args.headless,
+        args=["--new-tab"],
+    )
+    page = browser.pages[0]
+    return browser, page
+
+
+def _get_default_firefox_profile():
+    profile_path = path.expanduser("~/Library/Application Support/Firefox/Profiles")
+    if not path.exists(profile_path):
+        raise Exception("Firefox profiles directory not found.")
+
+    default_profiles = glob(path.join(profile_path, "*.default"))
+    if not default_profiles:
+        raise Exception("Default profile not found.")
+
+    return default_profiles[0]
+
+
+def get_current_user_id(page):
+    page.goto("https://sysdig.bizneohr.com")
+    time_xpath = "//a[contains(@class, 'menu-link')][contains(@data-active-link, 'time-attendance')]"
+    return page.locator(time_xpath).get_attribute("href").split("/")[-1]
+
+
+def add_expected_schedule_at_date_for_user(page, user_id, year, month, day):
+    page.goto(f"https://sysdig.bizneohr.com/time-attendance/my-logs/{user_id}?year={year}&month={month}")
+    page.reload()
+    page.locator(f"//tr[@data-bulk-element='{day}']/td[@class='actions']").click()
+    add_default_schedule_selector = f"//form[contains(@action, '={day}')]//button[contains(@class, 'is-link')][contains(text(), 'jornada esperada')]"  # noqa
+    if not any_locator_is_visible(page, add_default_schedule_selector):
+        print("Schedule was already registered")
+        return
+
+    print(f"adding expected schedule for user {user_id} at {year}-{month}-{day})")
+    for element in page.locator(add_default_schedule_selector).all():
+        if element.is_visible():
+            element.click()
+    page.locator("//button[contains(text(), 'Confirmar')]").click()
+    page.wait_for_selector("//*[contains(text(), 'Has añadido con éxito')]").wait_for_element_state("visible")
+
+
+def any_locator_is_visible(page, selector):
+    return any([x.is_visible() for x in page.locator(selector).all()])
